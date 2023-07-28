0d5d6324a64ed14344d3c6053888ef0c406a2c0f
Rahul14052004 <131978363+Rahul14052004@users.noreply.github.com>
2023-07-28 12:50:31 +0530

    initialising server and socket

--- server.java
+++ server.java
@@ -1,1 +1,23 @@
+import java.net.*;
+
+
+public class server{
+    ServerSocket server;
+    Socket socket;
+    public server(){
+        try{
+            server =new ServerSocket();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String args[]){
+        System.out.println("Server...");
+
+        new server();
+
+    }
+}
 
