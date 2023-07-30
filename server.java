9484dc2700ee1aafb3db38aa073f25c3de0fb8e4
Rahul14052004 <131978363+Rahul14052004@users.noreply.github.com>
2023-07-30 11:31:43 +0530

    Made the message reader.

--- server.java
+++ server.java
@@ -1,15 +1,52 @@
 import java.net.*;
+import java.io.*;
 
 
 public class server{
     ServerSocket server;
     Socket socket;
+    BufferedReader br;
+    PrintWriter pw;
     public server(){
         try{
-            server =new ServerSocket();
+            server =new ServerSocket(7777);
+            System.out.println("Ready to accept connection!");
+            System.out.println(".......");
+            server.accept();
+            br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            pw=new PrintWriter(socket.getOutputStream());
+
+            begin_reading();
         }
         catch(Exception e){
             e.printStackTrace();
+        }
+    }
+
+    public void begin_reading(){
+        Runnable r1=()->{
+            System.out.println("Reading...");
+            while(true){
+                try{
+                String message=br.readLine();
+                if(message.equals("finish.")){
+                    System.out.println("Client terminated.");
+                    break;
+                }
+                System.out.println("User2:"+message);
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+
+        
+        }
+    }
+}
+    public void begin_writing(){
+        Runnable r2=()->{
+
         }
     }
 
@@ -20,4 +57,3 @@ public class server{
 
     }
 }
-
