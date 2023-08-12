9460b85938a21032c60544031c4af643487bc25f
Rahul14052004 <131978363+Rahul14052004@users.noreply.github.com>
2023-08-12 14:35:00 +0530

    Started GUI changes

--- client.java
+++ client.java
@@ -1,22 +1,47 @@
 import java.io.*;
 import java.net.*;
+import javax.swing.*;
+import java.awt.*;
 
 public class client {
     Socket socket;
     BufferedReader br;
     PrintWriter pw;
+    JFrame main_frame;
+    JPanel chat_panel;
+    JLabel label1,label2;
+    JTextField text_box;
     public client(){
         try{
-            System.out.println("Sending request to server");
+            main_frame=new JFrame("Chat");
+            chat_panel= new JPanel();
+            chat_panel.setBounds(0,100,100,100);
+            chat_panel.setBackground(Color.gray);
+            label1= new JLabel("Request sent to server");
+            label1.setBounds(20,20,100,30);
             socket = new Socket("127.0.0.1",7777);
-            System.out.println("Connected");
+            label2= new JLabel("Connected");
+            label2.setBounds(20,40,100,30);
+            main_frame.add(label1);main_frame.add(label2);
 
             br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
             pw=new PrintWriter(socket.getOutputStream());
-
+            
             begin_reading();
             begin_writing();
+
+
+
+
+
+
+            main_frame.add(chat_panel,BorderLayout.CENTER);
+            main_frame.setSize(100,200);
+            main_frame.setVisible(true);
+            main_frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+
         }catch(Exception e){
             e.printStackTrace();
         }
@@ -26,6 +51,7 @@ public class client {
             System.out.println("Reading...");
             while(true){
                 try{
+                
                 String message=br.readLine();
                 if(message.equals("finish.")){
                     System.out.println("Client terminated.");
@@ -66,4 +92,3 @@ public class client {
     }
 
 }
-
